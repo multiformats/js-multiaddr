5b965d4a5016358f9ae256a9437bc3dfdf23e73b
David Dias <daviddias.p@gmail.com>
2016-03-11 21:12:52 +0000

    move tests to mocha, set up karma, webpack and dist npm script

--- tests/test.js
+++ tests/test.js
@@ -1,49 +1,69 @@
-var test = require('tape')
-var bufeq = require('buffer-equal')
-var m = require('../src')
+/* eslint-env mocha */
 
-test('construction', function (t) {
-  var a = m('/ip4/127.0.0.1/udp/1234')
-  t.ok(m('/ip4/127.0.0.1/udp/1234') instanceof m, 'construction')
-  t.ok(m(a) !== a, 're-construct should copy')
+const multiaddr = require('../src')
+const expect = require('chai').expect
 
-  // reconstruct with buffer
-  t.ok(m(a.buffer).buffer !== a.buffer, 're-construct should copy')
-  t.ok(bufeq(m(a.buffer).buffer, a.buffer), 're-construct should be equal buffer')
+describe('construction', (done) => {
+  var udpAddr
 
-  // reconstruct with string
-  t.ok(m(a.toString()).buffer !== a.buffer, 're-construct should copy')
-  t.ok(bufeq(m(a.toString()).buffer, a.buffer), 're-construct should be equal buffer')
+  it('create multiaddr', (done) => {
+    udpAddr = multiaddr('/ip4/127.0.0.1/udp/1234')
+    expect(udpAddr instanceof multiaddr).to.equal(true)
+    done()
+  })
 
-  // reconstruct with object
-  t.ok(m(a).buffer !== a.buffer, 're-construct should copy')
-  t.ok(bufeq(m(a).buffer, a.buffer), 're-construct should be equal buffer')
+  it('clone multiaddr', (done) => {
+    const udpAddrClone = multiaddr(udpAddr)
+    expect(udpAddrClone !== udpAddr).to.equal(true)
+    done()
+  })
 
-  // empty construct still works :)
-  t.equal(m('').toString(), '/', 'construct empty works')
+  it('reconstruct with buffer', (done) => {
+    expect(multiaddr(udpAddr.buffer).buffer === udpAddr.buffer).to.equal(false)
+    expect(multiaddr(udpAddr.buffer).buffer).to.deep.equal(udpAddr.buffer)
+    done()
+  })
 
-  t.end()
+  it('reconstruct with string', (done) => {
+    expect(multiaddr(udpAddr.toString()).buffer === udpAddr.buffer).to.equal(false)
+    expect(multiaddr(udpAddr.toString()).buffer).to.deep.equal(udpAddr.buffer)
+    done()
+  })
+
+  it('reconstruct with object', (done) => {
+    expect(multiaddr(udpAddr).buffer === udpAddr.buffer).to.equal(false)
+    expect(multiaddr(udpAddr).buffer).to.deep.equal(udpAddr.buffer)
+    done()
+  })
+
+  it('empty construct still works', (done) => {
+    expect(multiaddr('').toString()).to.equal('/')
+    done()
+  })
 })
 
-test('basic', function (t) {
-  var s = '/ip4/127.0.0.1/udp/1234'
-  var b = new Buffer('047f0000011104d2', 'hex')
-  var a = m(s)
-  t.equal(a.toString(), s, '.toString')
-  t.ok(bufeq(a.buffer, b), '.buffer')
+describe('manipulation', () => {
+  it('basic', (done) => {
+    const udpAddrStr = '/ip4/127.0.0.1/udp/1234'
+    const udpAddrBuf = new Buffer('047f0000011104d2', 'hex')
+    const udpAddr = multiaddr(udpAddrStr)
 
-  t.deepEqual(a.protoCodes(), [4, 17], '.protoCodes')
-  t.deepEqual(a.protoNames(), ['ip4', 'udp'], '.protoNames')
-  t.deepEqual(a.protos(), [m.protocols.codes[4], m.protocols.codes[17]], '.protos')
-  t.ok(a.protos()[0] !== m.protocols.codes[4], '.protos copies')
+    expect(udpAddr.toString()).to.equal(udpAddrStr)
+    expect(udpAddr.buffer).to.deep.equal(udpAddrBuf)
 
-  b = a.encapsulate('/udp/5678')
-  t.equal(b.toString(), '/ip4/127.0.0.1/udp/1234/udp/5678', '.encapsulate')
-  t.equal(b.decapsulate('/udp').toString(), '/ip4/127.0.0.1/udp/1234', '.decapsulate')
-  t.equal(b.decapsulate('/ip4').toString(), '/', '.decapsulate')
-  t.throws(function () { a.decapsulate('/').toString() }, undefined, '.decapsulate / throws')
-  t.equal(m('/').encapsulate(a).toString(), a.toString(), '.encapsulate empty works')
-  t.equal(m('/').decapsulate('/').toString(), '/', '.decapsulate empty works')
+    expect(udpAddr.protoCodes()).to.deep.equal([4, 17])
+    expect(udpAddr.protoNames()).to.deep.equal(['ip4', 'udp'])
+    expect(udpAddr.protos()).to.deep.equal([multiaddr.protocols.codes[4], multiaddr.protocols.codes[17]])
+    expect(udpAddr.protos()[0] === multiaddr.protocols.codes[4]).to.equal(false)
 
-  t.end()
+    const udpAddrBuf2 = udpAddr.encapsulate('/udp/5678')
+    expect(udpAddrBuf2.toString()).to.equal('/ip4/127.0.0.1/udp/1234/udp/5678')
+    expect(udpAddrBuf2.decapsulate('/udp').toString()).to.equal('/ip4/127.0.0.1/udp/1234')
+    expect(udpAddrBuf2.decapsulate('/ip4').toString()).to.equal('/')
+    expect(function () { udpAddr.decapsulate('/').toString() }).to.throw
+    expect(multiaddr('/').encapsulate(udpAddr).toString()).to.equal(udpAddr.toString())
+    expect(multiaddr('/').decapsulate('/').toString()).to.equal('/')
+
+    done()
+  })
 })
